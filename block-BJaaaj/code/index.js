5256c93a13d2e82b41d83bd06931a8e0c3fcdc72
KASHISH <kashu80525@gmail.com>
2023-04-07 13:14:53 +0530

    making corrections on code

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -3,10 +3,6 @@
 let firstName = "Arya";
 let lastName = "Stark";
 
-firstName="Arya";
-firstName;
-lastName="Stark";
-lastName;
 
 // Do the following:
 
@@ -19,20 +15,17 @@ firstName="John";
 // Assign a new value of `"Snow"` to lastName.
 lastName="Snow";
 // Alert the new value of `firstName` and `lastName` by concatenating them
-alert(firstName);
-alert(lastName);
+alert(firstName + lastName);
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
-let fullName= firstName + " " + lastName;
+const fullName= firstName + " " + lastName;
 alert(fullName);
 // Declare three variables `name,` `newUser` and `isAdmin`.
-let Name="kashish";
-let newUser="kashish";
-let isAdmin=true;
+let Name,newUser,isAdmin;
 // Assign your name as the value of `name`.
- Name="kashish";
+ Name= "kashish";
 
 // Assign the value of `name` to `newUser`.
-newUser="kashish";
+newUser= Name;
 // Set the value of `isAdmin` to `true`.
  isAdmin=true;
 // Alert the values of all the three variables.
@@ -48,16 +41,12 @@ var wiseMan = "Tyrion Lannister";
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
 wiseMan= "Samwell Tarly";
-wiseMan;
 // Declare a variable `userName` with value "Lysa Arryn"
 let userName=" Lysa Arryn";
 // Declare a variable named `oddNumber` and assign a value `57`.
-let oddNumber;
-oddNumber=57;
-oddNumber;
+let oddNumber=57;
 // Reassign the value of `oddNumber` to 61
 oddNumber=61;
-oddNumber;
 // Mathematical Operation
 
 let amount = 4280;
@@ -66,17 +55,14 @@ let amount = 4280;
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
 let reducedAmount;
-reducedAmount=4256;
-alert(reducedAmount);
+reducedAmount= amount-24;
+
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
 let addedAmount;
-addedAmount=4312;
-alert(addedAmount);
+addedAmount= amount+32
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
 let multipleAmount;
-multipleAmount=29960;
-alert(multipleAmount);
+multipleAmount= amount * 7;
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
 let dividedAmount;
-dividedAmount=75.09;
-alert(dividedAmount);
+dividedAmount= amount/ 57;
